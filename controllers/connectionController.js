e98176069162f54a3e39a60e1c60e91a38283579
William Stewart <wstewa25@uncc.edu>
2021-11-16 13:15:22 -0500

    updated connectionController.js

--- controllers/connectionController.js
+++ controllers/connectionController.js
@@ -1,10 +1,5 @@
 const model = require('../models/connection');
 
-//GET /connections/new: send html form for creating a new connection
-exports.new = (req, res) => {
-    res.render('./connection/new');
-};
-
 //GET /connections sends the connections page
 exports.index = (req, res, next) => {
     model.find()
@@ -12,10 +7,15 @@ exports.index = (req, res, next) => {
         .catch(err => next(err));
 };
 
+//GET /connections/new: send html form for creating a new connection
+exports.new = (req, res) => {
+    res.render('./connection/new');
+};
+
 //POST /connections: create a new connection
 exports.create = (req, res, next) => {
     let connection = new model(req.body);
-
+    connection.author = req.session.user;
     connection.save()
         .then(connection => res.redirect('/connections'))
         .catch(err => {
@@ -30,12 +30,8 @@ exports.create = (req, res, next) => {
 //GET /connections/:id: send details of connection identified by id
 exports.show = (req, res, next) => {
     let id = req.params.id;
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        let err = new Error('Invalid connection id');
-        err.status = 400;
-        return next(err);
-    }
-    model.findById(id)
+
+    model.findById(id).populate('author', 'firstName lastName')
         .then(connection => {
             if (connection) {
                 return res.render('./connection/show', { connection });
@@ -51,11 +47,7 @@ exports.show = (req, res, next) => {
 //GET /connections/:id/edit: send form for editing an existing connection
 exports.edit = (req, res, next) => {
     let id = req.params.id;
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        let err = new Error('Invalid connection id');
-        err.status = 400;
-        return next(err);
-    }
+
     model.findById(id)
         .then(connection => {
             if (connection) {
@@ -74,11 +66,6 @@ exports.update = (req, res, next) => {
     let connection = req.body;
     let id = req.params.id;
 
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        let err = new Error('Invalid connection id');
-        err.status = 400;
-        return next(err);
-    }
     model.findByIdAndUpdate(id, connection, { useFindAndModify: false, runValidators: true })
         .then(connection => {
             if (connection) {
@@ -99,12 +86,6 @@ exports.update = (req, res, next) => {
 //DELETE /connections/:id: delete connection identified by id
 exports.delete = (req, res, next) => {
     let id = req.params.id;
-
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        let err = new Error('Invalid connection id');
-        err.status = 400;
-        return next(err);
-    }
 
     model.findByIdAndDelete(id, { useFindAndModify: false })
         .then(connection => {
