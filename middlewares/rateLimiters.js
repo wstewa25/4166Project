8ce973a9a9c3fcbfd6d6ad36fb97929da77f8128
William Stewart <wstewa25@uncc.edu>
2021-12-02 18:02:48 -0500

    added rate limiter for login

---
+++ middlewares/rateLimiters.js
@@ -0,0 +1,11 @@
+const rateLimit = require("express-rate-limit");
+exports.logInLimiter = rateLimit ({
+    windowMs: 60 * 1000,
+    max: 5,
+    // message: "Too many login requrests. Try again later."
+    handler: (req, res, next) => {
+        let err = new Error('Too many login requests. Try again later.');
+        err.status = 429;
+        return next(err);
+    }
+});
