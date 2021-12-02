cc328e1aec8c5739d0aa58418e4f8ec4391a8b2d
William Stewart <wstewa25@uncc.edu>
2021-12-02 18:00:37 -0500

    added validators to validator.js

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -12,4 +12,35 @@ exports.validateId = (req, res, next) => {
     }
 }
 
+exports.validateSignUp = [body('firstName', 'First name cannot be empty').notEmpty().trim().escape(),
+body('lastName', 'Last name cannot be empty').notEmpty().trim().escape(),
+body('email', 'Email must be a valid email address').isEmail().trim().normalizeEmail(),
+body('password', 'Password must be at least 8 characters and at most 64 characters').isLength({min: 8, max: 64})];
+
+exports.validateLogIn = [body('email','Email must be a valid email address').isEmail().trim().normalizeEmail(),
+body('password', 'Password must be at least 8 characters and at most 64 characters').isLength({min: 8, max: 64})];
+
 exports.validateRsvp = [body('rsvp').isIn(['YES', 'NO', 'MAYBE'])];
+
+exports.validateResult = (req, res, next) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        errors.array().forEach(error=> {
+            req.flash('error', error.msg);
+        });
+        return res.redirect('back');
+    } else {
+        return next();
+    }
+}
+
+exports.validateConnection = [
+    body('name', 'Connection Name cannot be empty').notEmpty().trim().escape(),
+    body('details', 'Connection Details must be at least 10 characters in length').trim().escape().isLength({min: 10}),
+    body('category', 'Connection Category cannot be empty').notEmpty().trim().escape(),
+    body('date', 'Connection Date cannot be empty').notEmpty().trim().escape(),
+    body('start_time', 'Connection start time cannot be empty').notEmpty().trim().escape(),
+    body('end_time', 'Connection end time cannot be empty').notEmpty().trim().escape(),
+    body('image', 'Connection image link cannot be empty').notEmpty().trim().escape(),
+    body('location', 'Connection location cannot be empty').notEmpty().trim().escape(),
+];
